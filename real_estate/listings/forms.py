c7c36f2ccd3d6f539494aa79a0713bf6a6e2b5d5
hiennm <hiennm11599@gmail.com>
2022-07-04 11:43:17 +0700

    first commit

---
+++ real_estate/listings/forms.py
@@ -0,0 +1,17 @@
+from dataclasses import field
+from pyexpat import model
+from django.forms import ModelForm
+from .models import Listing
+
+class ListingForm(ModelForm):
+    class Meta:
+        model = Listing
+        fields = [
+            "title",
+            "price",
+            "num_bedrooms",
+            "num_bathrooms",
+            "square_footage",
+            "address",
+            "image",
+        ]
---
+++ real_estate/listings/models.py
@@ -0,0 +1,20 @@
+from cgi import print_exception
+from ctypes import addressof
+from turtle import title
+from django.db import models
+
+# Create your models here.
+
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.IntegerField()
+    num_bedrooms = models.IntegerField()
+    num_bathrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    address = models.CharField(max_length=200)
+    image = models.ImageField() 
+
+    def __str__(self):
+        return self.title
